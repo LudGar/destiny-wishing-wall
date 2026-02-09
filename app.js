ff9e0e5d8f3d3b528c6e54bf6a6f0ca6be6a9243
Lud Gar <40274822+LudGar@users.noreply.github.com>
2026-02-09 22:00:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const SYMBOLS = [
   { id: 13, name: 'Owl',             row: 3, col: 1 },
   { id: 14, name: 'Eagle Head',      row: 3, col: 2 },
   { id: 15, name: 'Eagle',           row: 3, col: 3 },
-  { id: 16, name: 'Riven', standalone: true }   // Standalone symbol (NOT part of atlas grid)
+  { id: 16, name: 'Riven',           standalone: true }   // Standalone symbol (NOT part of atlas grid)
 ];
 
 
@@ -31,88 +31,80 @@ const WISHES = [
     title: "Grants an etherial key, used in raid treasure room.",
     description: "A wish to feed an addiction.",
     pattern: [
-      0, 0, 13, 13, 13,
-      0, 0, null, 13, 13,
-      5, 5, null, 3, 3,
-      5, 5, 9, 3, 3
-    ]
+         0,    0,   13,   13,   13,
+         0,    0, null,   13,   13,
+         5,    5, null,    3,    3,
+         5,    5,    9,    3,    3]
   },
   {
     number: 2,
     title: "Spawns chest between third and fourth encounters.",
     description: "A wish for material validation.",
     pattern: [
-      3, null, 10, null, 2,
-      3, null, 10, null, 2,
-      1, null, 9,  null, 2,
-      1, null, 9,  null, 4
-    ]
+         3, null,   10, null,    2,
+         3, null,   10, null,    2,
+         1, null,    9, null,    2,
+         1, null,    9, null,    4]
   },
   {
     number: 3,
     title: "Awards 'Numbers of Power' emblem.",
     description: "A wish for others to celebrate your success.",
     pattern: [
-      0, null, 0, null, 0,
-      null, 4, null, 4, null,
-      8, null, 8, null, 8,
-      null, 12, null, 12, null
-    ]
+         0, null,    0, null,    0,
+      null,    4, null,    4, null,
+         8, null,    8, null,    8,
+      null,   12, null,   12, null]
   },
   {
     number: 4,
     title: "Warp to second encounter (Shuro Chi).",
     description: "A wish to look athletic and elegant.",
     pattern: [
-      14, 15, 15, 15, 12,
-      6,  14, 4,  12, 6,
-      6,  12, null, 14, 6,
-      12, 5,  5,  5,  14
-    ]
+        14,   15,   15,   15,   12,
+         6,   14,    4,   12,    6,
+         6,   12, null,   14,    6,
+        12,    5,    5,    5,   14]
   },
   {
     number: 5,
     title: "Warp to third encounter (Morgeth).",
     description: "A wish for a promising future.",
     pattern: [
-      4, 11, 14, 11, 4,
-      null, 8, 15, 8, null,
-      null, 8, 14, 8, null,
-      4, 11, 14, 11, 4
-    ]
+         4,   11,   14,   11,    4,
+      null,    8,   15,    8, null,
+      null,    8,   14,    8, null,
+         4,   11,   14,   11,    4]
   },
   {
     number: 6,
     title: "Warp to fourth encounter (The Vault).",
     description: "A wish to move the hands of time.",
     pattern: [
-      9,  12, 15, 12, 9,
-      0,  9,  null, 9,  0,
-      0,  null, null, null, 0,
-      10, 10, 10, 10, 10
-    ]
+         9,   12,   15,   12,    9,
+         0,    9, null,    9,    0,
+         0, null, null, null,    0,
+        10,   10,   10,   10,   10]
   },
   {
     number: 7,
     title: "Warp to final encounter (Riven).",
     description: "A wish to help a friend in need.",
     pattern: [
-      15, 3,  11, null, 5,
-      8,  null, null, 6,  14,
-      8,  null, 13, 6,  14,
-      15, 3,  11, null, 5
-    ]
+        15,    3,   11, null,    5,
+         8, null, null,    6,   14,
+         8, null,   13,    6,   14,
+        15,    3,   11, null,    5]
   },
   {
     number: 8,
     title: "Plays song 'Hope for the Future'.",
     description: "A wish to stay here forever.",
     pattern: [
-      null, 11, 13, 10, null,
-      null, 11, 13, 10, null,
-      null, 11, 15, 10, null,
-      null, 11, 13, 10, null
-    ]
+      null,   11,   13,   10, null,
+      null,   11,   13,   10, null,
+      null,   11,   15,   10, null,
+      null,   11,   13,   10, null]
   },
   {
     number: 9,
@@ -120,76 +112,69 @@ const WISHES = [
     description: "A wish to stay here forever.",
     pattern: [
       null, null, null, null, null,
-      null, 1, 1, 1, null,
-      null, 1, 1, 13, null,
-      null, null, null, null, null
-    ]
+      null,    1,    1,    1, null,
+      null,    1,    1,   13, null,
+      null, null, null, null, null]
   },
   {
     number: 10,
     title: "Adds Drifter voicelines to moments in the raid.",
     description: "A wish to stay here forever.",
     pattern: [
-      1, 1, null, null, 1,
-      1, 11, 11, null, 3,
-      1, null, 6, 6, 3,
-      null, null, null, 3, 3
-    ]
+         1,    1, null, null,    1,
+         1,   11,   11, null,    3,
+         1, null,    6,    6,    3,
+      null, null, null,    3,    3]
   },
   {
     number: 11,
     title: "Grunt birthday party effect within the raid.",
     description: "A wish to stay here forever..",
     pattern: [
-      14, 14, null, 15, 15,
-      14, null, 1, 4, 15,
-      12, null, 1, null, 5,
-      12, 12, null, 5, 5
-    ]
+        14,   14, null,   15,   15,
+        14, null,    1,    4,   15,
+        12, null,    1, null,    5,
+        12,   12, null,    5,    5]
   },
   {
     number: 12,
     title: "Adds unique effects to the heads of fireteam members.",
     description: "A wish to open your mind to new ideas.",
     pattern: [
-      0, null, null, null, 8,
-      0, 0, 8, 8, 8,
-      3, 3, 11, 14, 14,
-      3, null, null, null, 14
-    ]
+         0, null, null, null,    8,
+         0,    0,    8,    8,    8,
+         3,    3,   11,   14,   14,
+         3, null, null, null,   14]
   },
   {
     number: 13,
     title: "Unlocks 'Petra's Run' Version of the raid (Flawless Mode).",
     description: "A wish for the means to feed an addiction.",
     pattern: [
-      12, 1, 13, 14, 7,
-      12, 1, null, 14, 7,
-      7,  14, null, 1,  12,
-      7,  14, null, 1,  12
-    ]
+        12,    1,   13,   14,   7,
+        12,    1, null,   14,   7,
+         7,   14, null,    1,  12,
+         7,   14, null,    1,  12]
   },
   {
     number: 14,
     title: "Spawns Ahamkara eggs throughout the raid.",
     description: "A wish for love and support.",
     pattern: [
-      4, 9, 13, 8, 4,
-      null, null, 15, null, null,
-      0, null, 5, 7, null,
-      4, 11, null, null, 10
-    ]
+         4,    9,   13,    8,    4,
+      null, null,   15, null, null,
+         0, null,    5,    7, null,
+         4,   11, null, null,   10]
   },
   {
     number: 15,
     title: "The Unknown Wish",
     description: "'This one you shall cherish.' -- Riven of a Thousand Voices",
     pattern: [
-      null, null, 16, null, null,
+      null, null,   16, null, null,
       null, null, null, null, null,
-      16, null, null, null, 16,
-      null, null, null, 16, null
-    ]
+        16, null, null, null,   16,
+      null, null, null,   16, null]
   }
 ];
 
